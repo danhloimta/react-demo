150fc094344e772e35a9dce76dc2f71f5a47f525
Danh Lợi <nguyen.danh.loi@sun-asterisk.com>
2020-09-30 09:40:51 +0700

    Updates

---
+++ precache-manifest.7fc896f747b006c9c371c3d5616f9a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2d3a69dc1f11557076cf7197e880e4",
+    "url": "http://localhost:3001/index.html"
+  },
+  {
+    "revision": "3acd8b446205fe5e8436",
+    "url": "http://localhost:3001/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "23678d32c36569f03a25",
+    "url": "http://localhost:3001/static/css/main.97a1ea9f.chunk.css"
+  },
+  {
+    "revision": "3acd8b446205fe5e8436",
+    "url": "http://localhost:3001/static/js/2.3e6fdb5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "http://localhost:3001/static/js/2.3e6fdb5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23678d32c36569f03a25",
+    "url": "http://localhost:3001/static/js/main.f238d7fa.chunk.js"
+  },
+  {
+    "revision": "bf3fe2dc789e0b00adb3",
+    "url": "http://localhost:3001/static/js/runtime-main.961eafbc.js"
+  }
+]);
